3ef32190da9ce67d67df1fcd00e35ad31d2d8ad0
Andrei19977 <andrey_tomchik@mail.ru>
2021-02-23 23:17:49 +0300

    feat: complete Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (Array.isArray(array) && array.length) {
+    array.sort((a, b) => a - b)
+    return array[0]
+  } else {
+    return 0
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        array.sort((a, b) => a - b)
+        return array[array.length - 1]
+      } else {
+        return 0
+      }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length) {
+        let sum=0
+        for (i=0; i<array.length; i++){
+    sum+=array[i]
+        }
+        return sum/array.length
+      } else {
+        return 0
+      }
 }
